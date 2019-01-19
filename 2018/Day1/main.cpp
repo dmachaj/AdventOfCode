25bafd7884ed88ae3c33d522c6ffc99eb5018d84
David Machaj <46852402+dmachaj@users.noreply.github.com>
2019-01-19 14:07:42 -0800

    Solve day 1 2018

--- 2018/Day1/main.cpp
+++ 2018/Day1/main.cpp
@@ -6,6 +6,14 @@
 
 int main()
 {
-    std::cout << 0 << std::endl;
+    int64_t runningTotal{};
+    std::string input;
+    while (std::getline(std::cin, input))
+    {
+        const auto number = std::atoi(input.c_str());
+        runningTotal += number;
+        // std::cout << "Read in " << number << " running total " << runningTotal << std::endl;
+    }
+    std::cout << runningTotal << std::endl;
     return 0;
 }
