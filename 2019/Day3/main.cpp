aae5021f79c175675eb849ad1d9fb09eb29a8f17
David Machaj <46852402+dmachaj@users.noreply.github.com>
2019-12-02 21:48:46 -0800

    Solve 2019 Day 3 Part 2

--- 2019/Day3/main.cpp
+++ 2019/Day3/main.cpp
@@ -33,6 +33,7 @@ namespace
     void PerformMoves(const std::vector<std::pair<char, uint32_t>>& moves, TLambda callback)
     {
         std::pair<int32_t, int32_t> currentLocation{};
+        uint32_t moveCount{};
         for (const auto move : moves)
         {
             auto movesRemaining = move.second;
@@ -55,7 +56,7 @@ namespace
                     default:
                         throw std::exception("wtf");
                 }
-                callback(currentLocation);
+                callback(currentLocation, ++moveCount);
             }
         }
     }
@@ -65,17 +66,16 @@ void Part1()
 {
     constexpr uint32_t c_gridDimensions{10000};
     std::vector<std::vector<uint32_t>> grid(c_gridDimensions * 2, std::vector<uint32_t>(c_gridDimensions * 2, 0));
-    grid.reserve(c_gridDimensions);
 
     const auto firstWire = ExtractInput();
-    PerformMoves(firstWire, [&](const std::pair<int32_t, int32_t>& currentLocation)
+    PerformMoves(firstWire, [&](const std::pair<int32_t, int32_t>& currentLocation, uint32_t)
     {
         grid[c_gridDimensions + currentLocation.first][c_gridDimensions + currentLocation.second] = true;
     });
 
     const auto secondWire = ExtractInput();
     std::vector<std::pair<int32_t, int32_t>> hits{};
-    PerformMoves(secondWire, [&](const std::pair<int32_t, int32_t>& currentLocation)
+    PerformMoves(secondWire, [&](const std::pair<int32_t, int32_t>& currentLocation, uint32_t)
     {
         if (grid[c_gridDimensions + currentLocation.first][c_gridDimensions + currentLocation.second])
         {
@@ -95,12 +95,38 @@ void Part1()
 
 void Part2()
 {
+    constexpr uint32_t c_gridDimensions{10000};
+    std::vector<std::vector<uint32_t>> grid(c_gridDimensions * 2, std::vector<uint32_t>(c_gridDimensions * 2, 0));
 
+    const auto firstWire = ExtractInput();
+    PerformMoves(firstWire, [&](const std::pair<int32_t, int32_t>& currentLocation, uint32_t moveCount)
+    {
+        grid[c_gridDimensions + currentLocation.first][c_gridDimensions + currentLocation.second] = moveCount;
+    });
+
+    const auto secondWire = ExtractInput();
+    std::vector<std::pair<int32_t, int32_t>> hits{};
+    PerformMoves(secondWire, [&](const std::pair<int32_t, int32_t>& currentLocation, uint32_t moveCount)
+    {
+        if (grid[c_gridDimensions + currentLocation.first][c_gridDimensions + currentLocation.second] != 0)
+        {
+            hits.emplace_back(std::make_pair(grid[c_gridDimensions + currentLocation.first][c_gridDimensions + currentLocation.second], moveCount));
+        }
+    });
+
+    std::vector<uint32_t> distances{};
+    for (const auto hit : hits)
+    {
+        distances.emplace_back(std::abs(hit.first) + std::abs(hit.second));
+    }
+    std::sort(distances.begin(), distances.end());
+
+    std::cout << distances[0] << std::endl;
 }
 
 int main()
 {
-    Part1();
-    // Part2();
+    // Part1();
+    Part2();
     return 0;
 }
