94228adc942424f985fc6a762b6a6c78dbbf28cf
David Machaj <46852402+dmachaj@users.noreply.github.com>
2019-12-04 20:56:49 -0800

    Update base template a bit

--- template.cpp
+++ template.cpp
@@ -3,9 +3,24 @@
 #include <cmath>
 #include <iostream>
 #include <string>
+#include <vector>
+
+namespace
+{
+    void Part1()
+    {
+        std::cout << 0 << std::endl;
+    }
+
+    void Part2()
+    {
+        std::cout << 0 << std::endl;
+    }
+}
 
 int main()
 {
-    std::cout << 0 << std::endl;
+    Part1();
+    // Part2();
     return 0;
 }
