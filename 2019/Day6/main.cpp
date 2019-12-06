54dcc84d8968344877efe7d4cd513ff82ca8ad43
David Machaj <46852402+dmachaj@users.noreply.github.com>
2019-12-05 21:31:54 -0800

    Solve 2019 Day 6 parts 1 and 2

---
+++ 2019/Day6/main.cpp
@@ -0,0 +1,124 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+
+namespace
+{
+    void Part1()
+    {
+        // first is orbiter, second is center
+        std::map<std::string, std::string> orbits;
+
+        std::string input;
+        while (std::getline(std::cin, input))
+        {
+            std::istringstream inputStream(input);
+            std::string center;
+            std::string orbiter;
+            std::getline(inputStream, center, ')');
+            std::getline(inputStream, orbiter);
+            
+            orbits[orbiter] = center;
+            // std::cerr << center << " orbited by " << orbiter << std::endl;
+        }
+
+        uint32_t numOrbits{};
+        for (const auto& object : orbits)
+        {
+            auto parent = orbits.find(object.second);
+            ++numOrbits;
+            while (parent != orbits.end())
+            {
+                ++numOrbits;
+                parent = orbits.find(parent->second);
+            }
+        }
+
+        std::cout << numOrbits << std::endl;
+    }
+
+    void Part2()
+    {
+        // first is orbiter, second is center
+        std::map<std::string, std::string> orbits;
+
+        std::string input;
+        while (std::getline(std::cin, input))
+        {
+            std::istringstream inputStream(input);
+            std::string center;
+            std::string orbiter;
+            std::getline(inputStream, center, ')');
+            std::getline(inputStream, orbiter);
+            
+            orbits[orbiter] = center;
+            // std::cerr << center << " orbited by " << orbiter << std::endl;
+        }
+
+        std::vector<std::string> myAncestors{};
+        {
+            auto parent = orbits.find("YOU");
+            while (parent != orbits.end())
+            {
+                myAncestors.emplace_back(parent->second);
+                parent = orbits.find(parent->second);
+            }
+        }
+
+        std::vector<std::string> santasAncestors{};
+        {
+            auto parent = orbits.find("SAN");
+            while (parent != orbits.end())
+            {
+                santasAncestors.emplace_back(parent->second);
+                parent = orbits.find(parent->second);
+            }
+        }
+
+        std::string firstCommonAncestor{};
+        for (const auto& myAncestor : myAncestors)
+        {
+            const auto result = std::find(santasAncestors.begin(), santasAncestors.end(), myAncestor);
+            if (result != santasAncestors.end())
+            {
+                firstCommonAncestor = *result;
+                break;
+            }
+        }
+
+        uint32_t transfers{};
+        {
+            auto parent = orbits.find("YOU");
+            while (parent->second != firstCommonAncestor)
+            {
+                ++transfers;
+                parent = orbits.find(parent->second);
+            }
+        }
+
+        {
+            auto parent = orbits.find("SAN");
+            while (parent->second != firstCommonAncestor)
+            {
+                ++transfers;
+                parent = orbits.find(parent->second);
+            }
+        }
+
+        // transfers--;
+
+        std::cout << transfers << std::endl;
+    }
+}
+
+int main()
+{
+    // Part1();
+    Part2();
+    return 0;
+}
