6142f57449c7e3611df0e96057d9eff6a08908c2
David Machaj <46852402+dmachaj@users.noreply.github.com>
2019-12-09 23:53:46 -0800

    Advent of code 2019 day 10 part 2 (fail)

--- 2019/Day10/main.cpp
+++ 2019/Day10/main.cpp
@@ -18,9 +18,9 @@ namespace
         }
     }
 
-    uint32_t CheckVisibility(std::vector<bool> const& asteroidField, int32_t rows, int32_t columns, int32_t startX, int32_t startY)
+    template<typename TLambda>
+    void CheckVisibility(std::vector<bool> const& asteroidField, int32_t rows, int32_t columns, int32_t startX, int32_t startY, TLambda callback)
     {
-        uint32_t visibleOthers{};
         for (auto x = 0; x < columns; ++x)
         {
             for (auto y = 0; y < rows; ++y)
@@ -44,7 +44,7 @@ namespace
                         tempY += rise;
                         if ((tempX == startX) && (tempY == startY))
                         {
-                            visibleOthers++;
+                            callback(x, y);
                             break;
                         }
                         else if (asteroidField[tempY * columns + tempX])
@@ -56,70 +56,159 @@ namespace
                 }
             }
         }
-        return visibleOthers;
     }
 
-    void Part1()
+    struct AsteroidField
     {
         std::vector<bool> asteroidField{};
-        std::string input;
         int32_t rows{};
+        int32_t columns{};
+    };
+
+    AsteroidField ReadInput()
+    {
+        AsteroidField result{};
+        std::string input;
         while (std::getline(std::cin, input))
         {
             for (const auto& cell : input)
             {
                 if (cell == '#')
                 {
-                    asteroidField.emplace_back(true);
+                    result.asteroidField.emplace_back(true);
                 }
                 else
                 {
-                    asteroidField.emplace_back(false);
+                    result.asteroidField.emplace_back(false);
                 }
             }
-            ++rows;
+            ++result.rows;
         }
-        int32_t columns = (int32_t)asteroidField.size() / rows;
+        result.columns = (int32_t)result.asteroidField.size() / result.rows;
+        return result;
+    }
 
+    struct VisibilityResult
+    {
+        std::pair<int32_t, int32_t> bestLocation{};
+        uint32_t numVisible;
+    };
+
+    VisibilityResult FindBestAsteroid(const AsteroidField& data)
+    {
         uint32_t maxVisible{};
-        for (auto x = 0; x < columns; ++x)
+        std::pair<int32_t, int32_t> bestLocation{};
+        for (auto x = 0; x < data.columns; ++x)
         {
-            for (auto y = 0; y < rows; ++y)
+            for (auto y = 0; y < data.rows; ++y)
             {
-                if (asteroidField[y * columns + x])
+                if (data.asteroidField[y * data.columns + x])
                 {
-                    const auto visible = CheckVisibility(asteroidField, rows, columns, x, y);
-                    if (visible > maxVisible)
+                    uint32_t countVisible{};
+                    CheckVisibility(data.asteroidField, data.rows, data.columns, x, y,
+                        [&](int32_t x, int32_t y)
+                        {
+                            x;y;
+                            countVisible++;
+                        });
+                    if (countVisible > maxVisible)
                     {
-                        maxVisible = visible;
+                        maxVisible = countVisible;
+                        bestLocation = std::make_pair(x, y);
                     }
                 }
-                // std::cerr << x << y << " ";
             }
-            // std::cerr << std::endl;
         }
 
-        // for (const auto row : asteroidField)
-        // {
-        //     for (const auto cell: row)
-        //     {
-        //         std::cerr << cell;
-        //     }
-        //     std::cerr << std::endl;
-        // }
+        return { bestLocation, maxVisible };
+    }
 
-        std::cout << maxVisible << std::endl;
+    void Part1()
+    {
+        const auto data = ReadInput();
+        const auto result = FindBestAsteroid(data);
+        std::cout << result.numVisible << std::endl;
+    }
+
+    double VectorAngle(int32_t x, int32_t y)
+    {
+        // if (x == 0) // special cases
+        // {
+        //     return (y > 0) ? 90 : 270;
+        // }
+        // else if (y == 0) // special cases
+        // {
+        //     return (x >= 0) ? 0 : 180;
+        // }
+        
+        double magnitude = sqrt((y * y) + (x * x));
+        double riseUnit = (double)y / magnitude;
+        double runUnit = (double)x / magnitude;
+        // const auto angle = (int32_t)((atan2(riseUnit, runUnit) / 3.14) * 180);
+        return (atan2(riseUnit, runUnit) / 3.14) * 180;
+        // if (x < 0 && y < 0) // quadrant Ⅲ
+        // {
+        //     return 180 + angle;
+        // }
+        // else if (x < 0) // quadrant Ⅱ
+        // {
+        //     return 180 + angle; // it actually substracts
+        // }
+        // else if (y < 0) // quadrant Ⅳ
+        // {
+        //     return 270 + (90 + angle); // it actually substracts
+        // }
+        // return angle;
     }
 
     void Part2()
     {
-        std::cout << 0 << std::endl;
+        auto data = ReadInput();
+        const auto result = FindBestAsteroid(data);
+        std::cerr << "The best asteroid is " << result.bestLocation.first << "," << result.bestLocation.second << std::endl;
+        uint32_t asteroidsZapped{};
+        double currentAngle{90.0}; // 90 degrees is straight up on the unit circle
+        std::pair<int32_t, int32_t> currentZapTarget{};
+        while (asteroidsZapped < 200)
+        {
+            double smallestAngleDiff{360};
+            CheckVisibility(data.asteroidField, data.rows, data.columns, result.bestLocation.first, result.bestLocation.second,
+                [&](int32_t x, int32_t y)
+                {
+                    auto rise = (y - result.bestLocation.second) * -1; // negate because euclidean coords count up from the center not down from the top
+                    auto run = x - result.bestLocation.first;
+
+                    const auto angle = VectorAngle(run, rise);
+
+                    auto angleDiff = (currentAngle + 0.1) - angle;
+                    if (angleDiff < 0)
+                    {
+                        angleDiff += 360;
+                    }
+
+                    if (angleDiff < smallestAngleDiff)
+                    {
+                        smallestAngleDiff = angleDiff;
+                        currentZapTarget = std::make_pair(x, y);
+                    }
+                });
+            
+            currentAngle -= (smallestAngleDiff + 0.01);
+            if (currentAngle < 0)
+            {
+                currentAngle += 360;
+            }
+            data.asteroidField[currentZapTarget.second * data.columns + currentZapTarget.first] = false; // ZAP!
+            asteroidsZapped++;
+            std::cerr << asteroidsZapped << "th asteroid at " << currentZapTarget.first << "," << currentZapTarget.second << std::endl;
+        }
+        std::cout << (currentZapTarget.first * 100) + currentZapTarget.second << std::endl;
     }
 }
 
 int main()
 {
-    Part1();
-    // Part2();
-    return 0;
+    // Part1();
+    Part2();
+    // return 0;
 }
