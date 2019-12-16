ce5f858dd97386454c5e0384b4055f97732790f9
David Machaj <46852402+dmachaj@users.noreply.github.com>
2019-12-15 23:41:35 -0800

    Solve 2019 Day 16 Part 2. Runtime was 23 minutes so its a weak solve at best

--- 2019/Day16/main.cpp
+++ 2019/Day16/main.cpp
@@ -4,9 +4,39 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
+#include <numeric>
 
 namespace
 {
+    constexpr std::array<int32_t, 4> c_phase{ 0, 1, 0, -1 };
+    void FFT(std::vector<uint32_t>& digits, uint32_t iterationCount)
+    {
+        for (auto iterations = 0UL; iterations < iterationCount; ++iterations)
+        {
+            std::vector<uint32_t> digitsCopy = digits;
+            for (auto i = 0UL; i < digits.size(); ++i)
+            {
+                std::vector<int32_t> phaseDigits{};
+                auto phaseDigitOffset = 0UL;
+                while (phaseDigits.size() < (digits.size() + 1))
+                {
+                    phaseDigits.insert(phaseDigits.end(), (i + 1), c_phase[phaseDigitOffset % c_phase.size()]);
+                    phaseDigitOffset++;
+                }
+
+                int32_t runningTotal{};
+                for (auto j = 0UL; j < digits.size(); ++j)
+                {
+                    runningTotal += digits[j] * phaseDigits[j + 1];
+                }
+                digitsCopy[i] = std::abs(runningTotal) % 10;
+            }
+
+            digits = std::move(digitsCopy);
+        }
+    }
+
     void Part1()
     {
         std::string input;
@@ -21,32 +51,7 @@ namespace
             digits.emplace_back(std::atoi(temp.c_str()));
         }
 
-        std::array<int32_t, 4> phase { 0, 1, 0, -1 };
-
-        for (auto iterations = 0UL; iterations < iterationCount; ++iterations)
-        {
-            std::vector<uint32_t> digitsCopy = digits;
-            for (auto i = 0UL; i < digits.size(); ++i)
-            {
-                std::vector<int32_t> phaseDigits{};
-                auto phaseDigitOffset = 0UL;
-                while (phaseDigits.size() < (digits.size() + 1))
-                {
-                    phaseDigits.insert(phaseDigits.end(), (i + 1), phase[phaseDigitOffset % phase.size()]);
-                    phaseDigitOffset++;
-                }
-                phaseDigits.erase(phaseDigits.begin(), ++phaseDigits.begin()); // delete first entry
-
-                int32_t runningTotal{};
-                for (auto j = 0UL; j < digits.size(); ++j)
-                {
-                    runningTotal += digits[j] * phaseDigits[j];
-                }
-                digitsCopy[i] = std::abs(runningTotal) % 10;
-            }
-
-            digits = digitsCopy;
-        }
+        FFT(digits, iterationCount);
 
         for (auto i = 0UL; i < 8; ++i)
         {
@@ -57,13 +62,59 @@ namespace
 
     void Part2()
     {
-        std::cout << 0 << std::endl;
+        constexpr uint32_t iterationCount = 100;
+        constexpr uint32_t inputRepeatCount = 10000;
+
+        std::string input;
+        std::getline(std::cin, input);
+        std::vector<uint32_t> digitsOriginal{};
+        for (const auto& letter : input)
+        {
+            const std::string temp { letter };
+            digitsOriginal.emplace_back(std::atoi(temp.c_str()));
+        }
+
+        uint32_t resultOffset{};
+        {
+            std::ostringstream resultOffsetStr{};
+            for (auto i = 0UL; i < 7; ++i)
+            {
+                resultOffsetStr << digitsOriginal[i];
+            }
+            resultOffset = std::atoi(resultOffsetStr.str().c_str());
+        }
+
+        std::vector<uint32_t> digits{};
+        for (auto i = 0UL; i < inputRepeatCount; ++i)
+        {
+            digits.insert(digits.end(), digitsOriginal.begin(), digitsOriginal.end());
+        }
+
+        // All digits before our offset will zero out so we can safely drop them, reducing memcpy costs.
+        digits.erase(digits.begin(), digits.begin() + resultOffset);
+
+        for (auto iterations = 0UL; iterations < iterationCount; ++iterations)
+        {
+            for (auto i = 0UL; i < digits.size(); ++i)
+            {
+                // Any given digit is a running total of all digits that follow it.  For some reason std::accumulate
+                // is 6-10x faster than a simple for loop with the same number of operations.
+                int32_t runningTotal = std::accumulate(digits.begin() + i, digits.end(), 0);
+                digits[i] = runningTotal % 10;
+            }
+        }
+
+        for (auto i = 0UL; i < 8; ++i)
+        {
+            std::cout << digits[i];
+        }
+        std::cout << std::endl;
     }
 }
 
 int main()
 {
-    Part1();
-    // Part2();
+    // Part1();
+    Part2();
     return 0;
 }
