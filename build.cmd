4c663d0daa7ee72e4ec8a598e20675c8e7d9e2c2
David Machaj <46852402+dmachaj@users.noreply.github.com>
2019-12-20 00:03:55 -0800

    Increase default stack size just enough to pass recursion problem

--- build.cmd
+++ build.cmd
@@ -1,2 +1,3 @@
-cl /EHsc /nologo /W4 /WX /std:c++17 /Zi main.cpp
+cl /EHsc /nologo /W4 /WX /std:c++17 /Zi /F 8000000 main.cpp
 :: /O2 max speed
+:: /F 8000000 increase stack size to 8MB from 1MB to make recursion problems a little bit easier
