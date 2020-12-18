7a095fcfe74ee07294815f470f63b230f693dc47
David Machaj <46852402+dmachaj@users.noreply.github.com>
2020-12-17 23:25:01 -0800

    Go back and solve 2020 Day 17 part 2

--- 2020/Day17/main.cpp
+++ 2020/Day17/main.cpp
@@ -29,7 +29,6 @@ namespace
         int64_t x{};
         int64_t y{};
         int64_t z{};
-        
     };
 
     bool operator==(const Point3D& lhs, const Point3D& rhs)
@@ -69,9 +68,9 @@ namespace
 
     void UpdateActiveNeighbors(unordered_map<Point3D, Space, Point3DHash>& allPoints, const Point3D& pt)
     {
-        // if (!allPoints[pt].isActive) throw 0;
+        auto& currentSpace = allPoints[pt];
+        if (!currentSpace.isActive) throw 0;
 
-        uint32_t result{};
         for (auto x = -1; x <= 1; ++x)
         {
             for (auto y = -1; y <= 1; ++y)
@@ -134,9 +133,103 @@ namespace
         std::cout << result << std::endl;
     }
 
+    struct Point4D
+    {
+        Point4D(int64_t x, int64_t y, int64_t z, int64_t w):
+            x(x), y(y), z(z), w(w)
+        {
+        }
+        int64_t x{};
+        int64_t y{};
+        int64_t z{};
+        int64_t w{};
+    };
+
+    bool operator==(const Point4D& lhs, const Point4D& rhs)
+    {
+        return (lhs.x == rhs.x) && (lhs.y == rhs.y) && (lhs.z == rhs.z) && (lhs.w == rhs.w);
+    }
+
+    struct Point4DHash
+    {
+        size_t operator()(const Point4D& pt) const noexcept
+        {
+            auto h1 = std::hash<int64_t>{}(pt.x);
+            auto h2 = std::hash<int64_t>{}(pt.y);
+            auto h3 = std::hash<int64_t>{}(pt.z);
+            auto h4 = std::hash<int64_t>{}(pt.w);
+            return h1 ^ (h2 << 1) ^ (h3 << 2) ^ (h4 << 3);
+        }
+    };
+    
+    void UpdateActiveNeighborsPart2(unordered_map<Point4D, Space, Point4DHash>& allPoints, const Point4D& pt)
+    {
+        auto& currentSpace = allPoints[pt];
+        if (!currentSpace.isActive) throw 0;
+
+        for (auto x = -1; x <= 1; ++x)
+        {
+            for (auto y = -1; y <= 1; ++y)
+            {
+                for (auto z = -1; z <= 1; ++z)
+                {
+                    for (auto w = -1; w <= 1; ++w)
+                    {
+                        if (x == 0 && y == 0 && z == 0 && w == 0) continue;
+                        Point4D lookup(pt.x + x, pt.y + y, pt.z + z, pt.w + w);
+                        auto& space = allPoints[lookup];
+                        space.nearbyActives++; // also allocates!
+                    }
+                }
+            }
+        }
+    }
+
     void Part2()
     {
-        std::cout << 0 << std::endl;
+        unordered_map<Point4D, Space, Point4DHash> allPoints{};
+        uint32_t width{};
+        uint32_t height{};
+        std::string input;
+        while (std::getline(std::cin, input))
+        {
+            width = input.length();
+            for (auto x = 0UL; x < width; ++x)
+            {
+                const bool isActive = input[x] == '#';
+                const Point4D pt{x, height, 0, 0};
+                allPoints[pt].isActive = isActive; // also allocates!
+            }
+            ++height;
+        }
+
+        for (auto i = 0UL; i < 6; ++i)
+        {
+            std::vector<Point4D> pointsCopy;
+            for (auto& pt : allPoints)
+            {
+                if (pt.second.isActive)
+                {
+                    pointsCopy.emplace_back(pt.first);
+                }
+            }
+
+            for (auto& pt : pointsCopy)
+            {
+                UpdateActiveNeighborsPart2(allPoints, pt);
+            }
+
+            for (auto& pt : allPoints)
+            {
+                pt.second.UpdateActivation();
+            }
+        }
+
+        const auto result = std::accumulate(allPoints.begin(), allPoints.end(), 0UL, [](const uint32_t sum, const auto& point)
+        {
+            return point.second.isActive ? (sum + 1) : sum;
+        });
+        std::cout << result << std::endl;
     }
 }
 
