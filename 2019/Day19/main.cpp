02e75e1896c219e3471cd5791569694c178c7c2e
David Machaj <46852402+dmachaj@users.noreply.github.com>
2020-12-27 21:16:59 -0800

    Refactor day19 intcode into shared header.  Still passes

--- 2019/Day19/main.cpp
+++ 2019/Day19/main.cpp
@@ -6,194 +6,13 @@
 #include <vector>
 #include <unordered_map>
 #include "HelpfulInfrastructure.h"
+#include "../Intcode/Intcode.h"
 
 namespace
 {
-    struct ProgramState
-    {
-        std::vector<int64_t> program{};
-        uint64_t instructionCounter{};
-        int64_t relativeBasePointer{};
-    };
-
-    bool ParamIsImmediate(int64_t data, uint64_t offset)
-    {
-        if ((uint64_t)data >= offset)
-        {
-            const auto result = (data / offset % 10);
-            return (result == 1);
-        } 
-        return false;
-    }
-
-    bool ParamIsRelative(int64_t data, uint64_t offset)
-    {
-        if ((uint64_t)data >= offset)
-        {
-            const auto result = (data / offset % 10);
-            return (result == 2);
-        } 
-        return false;
-    }
-
-    std::vector<int64_t> ExecuteProgram(ProgramState& state, std::vector<uint64_t> inputs/*, bool returnOnOutput = false*/)
-    {
-        auto& program = state.program;
-        auto& instructionCounter = state.instructionCounter;
-        std::vector<int64_t> result{};
-        uint32_t inputCount{};
-
-        while (program[instructionCounter] != 99)
-        {
-            const auto opcode = program[instructionCounter] % 100;
-            const bool firstParamImmediate = ParamIsImmediate(program[instructionCounter], 100);
-            const bool firstParamRelative = ParamIsRelative(program[instructionCounter], 100);
-            const bool secondParamImmediate = ParamIsImmediate(program[instructionCounter], 1000);
-            const bool secondParamRelative = ParamIsRelative(program[instructionCounter], 1000);
-            const bool thirdParamImmediate = ParamIsImmediate(program[instructionCounter], 10000);
-            const bool thirdParamRelative = ParamIsRelative(program[instructionCounter], 10000);
-
-            switch (opcode)
-            {
-            case 1: // Add
-            {
-                const auto param1 = firstParamImmediate ? program[instructionCounter + 1] :
-                                    firstParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 1]] :
-                                    program[program[instructionCounter + 1]];
-                const auto param2 = secondParamImmediate ? program[instructionCounter + 2] :
-                                    secondParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 2]] :
-                                    program[program[instructionCounter + 2]];
-                const auto destination = thirdParamRelative ? state.relativeBasePointer + program[instructionCounter + 3] : program[instructionCounter + 3];
-                program[destination] = param1 + param2;
-                instructionCounter += 4;
-                break;
-            }
-            case 2: // Multiply
-            {
-                const auto param1 = firstParamImmediate ? program[instructionCounter + 1] :
-                                    firstParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 1]] :
-                                    program[program[instructionCounter + 1]];
-                const auto param2 = secondParamImmediate ? program[instructionCounter + 2] :
-                                    secondParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 2]] :
-                                    program[program[instructionCounter + 2]];
-                const auto destination = thirdParamRelative ? state.relativeBasePointer + program[instructionCounter + 3] : program[instructionCounter + 3];
-                program[destination] = param1 * param2;
-                instructionCounter += 4;
-                break;
-            }
-            case 3: // input param
-            {
-                const auto destination = firstParamRelative ? state.relativeBasePointer + program[instructionCounter + 1] : program[instructionCounter + 1];
-                program[destination] = inputs[inputCount++];
-                instructionCounter += 2;
-                break;
-            }
-            case 4: // output param
-            {
-                const auto param1 = firstParamImmediate ? program[instructionCounter + 1] :
-                                    firstParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 1]] :
-                                    program[program[instructionCounter + 1]];
-                result.emplace_back(param1);
-                instructionCounter += 2;
-                // Guess - return when second output is reached
-                if (result.size() == 2) { return result; }
-                break;
-            }
-            case 5: // jump if true
-            {
-                const auto param1 = firstParamImmediate ? program[instructionCounter + 1] :
-                                    firstParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 1]] :
-                                    program[program[instructionCounter + 1]];
-                const auto param2 = secondParamImmediate ? program[instructionCounter + 2] :
-                                    secondParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 2]] :
-                                    program[program[instructionCounter + 2]];
-                if (param1 != 0)
-                {
-                    instructionCounter = (uint64_t)param2;
-                }
-                else
-                {
-                    instructionCounter += 3;
-                }
-                break;
-            }
-            case 6: // jump if false
-            {
-                const auto param1 = firstParamImmediate ? program[instructionCounter + 1] :
-                                    firstParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 1]] :
-                                    program[program[instructionCounter + 1]];
-                const auto param2 = secondParamImmediate ? program[instructionCounter + 2] :
-                                    secondParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 2]] :
-                                    program[program[instructionCounter + 2]];
-                if (param1 == 0)
-                {
-                    instructionCounter = (uint64_t)param2;
-                }
-                else
-                {
-                    instructionCounter += 3;
-                }
-                break;
-            }
-            case 7: // less than
-            {
-                const auto param1 = firstParamImmediate ? program[instructionCounter + 1] :
-                                    firstParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 1]] :
-                                    program[program[instructionCounter + 1]];
-                const auto param2 = secondParamImmediate ? program[instructionCounter + 2] :
-                                    secondParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 2]] :
-                                    program[program[instructionCounter + 2]];
-                const auto destination = thirdParamRelative ? state.relativeBasePointer + program[instructionCounter + 3] : program[instructionCounter + 3];
-                program[destination] = (param1 < param2) ? 1 : 0;
-                instructionCounter += 4;
-                break;
-            }
-            case 8: // equals
-            {
-                const auto param1 = firstParamImmediate ? program[instructionCounter + 1] :
-                                    firstParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 1]] :
-                                    program[program[instructionCounter + 1]];
-                const auto param2 = secondParamImmediate ? program[instructionCounter + 2] :
-                                    secondParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 2]] :
-                                    program[program[instructionCounter + 2]];
-                const auto destination = thirdParamRelative ? state.relativeBasePointer + program[instructionCounter + 3] : program[instructionCounter + 3];
-                program[destination] = (param1 == param2) ? 1 : 0;
-                instructionCounter += 4;
-                break;
-            }
-            case 9: // relative base pointer adjustment
-            {
-                const auto param1 = firstParamImmediate ? program[instructionCounter + 1] :
-                                    firstParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 1]] :
-                                    program[program[instructionCounter + 1]];
-                state.relativeBasePointer += param1;
-                instructionCounter += 2;
-                break;
-            }
-            default:
-                std::cerr << "program[0] at time of crash = " << program[0] << std::endl;
-                throw std::exception("Invalid opcode");
-            }
-        }
-        return result;
-    }
-
-    constexpr size_t c_memorySize{10000};
     void Part1()
     {
-        std::vector<int64_t> program{};
-        program.reserve(c_memorySize);
-
-        std::string input;
-        while (std::getline(std::cin, input, ','))
-        {
-            program.emplace_back(std::atoll(input.c_str()));
-        }
-
-        for (auto i = program.size(); i < c_memorySize; ++i)
-        {
-            program.emplace_back(0);
-        }
+        auto state = Intcode::ParseProgram();
 
         constexpr auto c_width = 50;
         constexpr auto c_height = 50;
@@ -204,9 +23,9 @@ namespace
             bool anyXMatches{false};
             for (auto x = 0UL; x < c_width; ++x)
             {
-                ProgramState state {program, 0, 0};
+                Intcode::ProgramState stateCopy {state.program, 0, 0};
                 std::vector<uint64_t> programInput{x, y};
-                const auto result = ExecuteProgram(state, programInput);
+                const auto result = Intcode::ExecuteProgram(stateCopy, programInput);
                 if ((result.size() > 0) && (result[0] != 0))
                 {
                     results[y * c_width + x] = true;
@@ -245,9 +64,9 @@ namespace
             return cache->second;
         }
 
-        ProgramState state {program, 0, 0};
+        Intcode::ProgramState state {program, 0, 0};
         std::vector<uint64_t> programInput{x, y};
-        const auto intCodeResult = ExecuteProgram(state, programInput);
+        const auto intCodeResult = Intcode::ExecuteProgram(state, programInput);
         const bool result = ((intCodeResult.size() != 0) && (intCodeResult[0] != 0));
         resultCache.emplace((x * 10000) + y, result);
         return result;
@@ -275,39 +94,27 @@ namespace
 
     void Part2()
     {
-        std::vector<int64_t> program{};
-        program.reserve(c_memorySize);
-
-        std::string input;
-        while (std::getline(std::cin, input, ','))
-        {
-            program.emplace_back(std::atoll(input.c_str()));
-        }
-
-        for (auto i = program.size(); i < c_memorySize; ++i)
-        {
-            program.emplace_back(0);
-        }
+        auto state = Intcode::ParseProgram();
 
         uint32_t minX = 0UL;
         for (auto y = 0UL; y < c_height; ++y)
         {
             for (auto x = minX; x < c_width; ++x)
             {
-                if (IsSquareAffected(program, x, y))
+                if (IsSquareAffected(state.program, x, y))
                 {
-                    if ((x - 1 > 0) && !IsSquareAffected(program, x - 1, y))
+                    if ((x - 1 > 0) && !IsSquareAffected(state.program, x - 1, y))
                     {
                         minX = x;
                     }
 
-                    if (DoesSquareFitWithin(program, x, y))
+                    if (DoesSquareFitWithin(state.program, x, y))
                     {
                         const auto finalResult = (x * 10000) + y;
                         std::cout << finalResult << std::endl;
                         return;
                     }
-                    else if (!IsSquareAffected(program, x + 1, y))
+                    else if (!IsSquareAffected(state.program, x + 1, y))
                     {
                         break;
                     }
---
+++ 2019/Intcode/Intcode.h
@@ -0,0 +1,204 @@
+#pragma once
+
+namespace Intcode
+{
+    constexpr size_t c_memorySize{10000};
+
+    enum class OpCode
+    {
+        Add = 1,
+        Multiply = 2,
+        Input = 3,
+        Output = 4,
+        JumpIfTrue = 5,
+        JumpIfFalse = 6,
+        LessThan = 7,
+        Equals = 8,
+        AdjustRelativeBase = 9,
+        Terminate = 99
+    };
+
+    struct ProgramState
+    {
+        std::vector<int64_t> program{};
+        uint64_t instructionCounter{};
+        int64_t relativeBasePointer{};
+    };
+
+    bool ParamIsImmediate(int64_t data, uint64_t offset)
+    {
+        if ((uint64_t)data >= offset)
+        {
+            const auto result = (data / offset % 10);
+            return (result == 1);
+        } 
+        return false;
+    }
+
+    bool ParamIsRelative(int64_t data, uint64_t offset)
+    {
+        if ((uint64_t)data >= offset)
+        {
+            const auto result = (data / offset % 10);
+            return (result == 2);
+        } 
+        return false;
+    }
+
+    ProgramState ParseProgram()
+    {
+        std::vector<int64_t> program(c_memorySize, 0);
+
+        std::string input;
+        size_t offset{};
+        while (std::getline(std::cin, input, ','))
+        {
+            program[offset] = (std::atoll(input.c_str()));
+            ++offset;
+        }
+
+        return {std::move(program), 0, 0};
+    }
+
+    std::vector<int64_t> ExecuteProgram(ProgramState& state, std::vector<uint64_t> inputs/*, bool returnOnOutput = false*/)
+    {
+        auto& program = state.program;
+        auto& instructionCounter = state.instructionCounter;
+        std::vector<int64_t> result{};
+        uint32_t inputCount{};
+
+        while ((OpCode)program[instructionCounter] != OpCode::Terminate)
+        {
+            const auto opcode = (OpCode)(program[instructionCounter] % 100);
+            const bool firstParamImmediate = ParamIsImmediate(program[instructionCounter], 100);
+            const bool firstParamRelative = ParamIsRelative(program[instructionCounter], 100);
+            const bool secondParamImmediate = ParamIsImmediate(program[instructionCounter], 1000);
+            const bool secondParamRelative = ParamIsRelative(program[instructionCounter], 1000);
+            const bool thirdParamImmediate = ParamIsImmediate(program[instructionCounter], 10000);
+            const bool thirdParamRelative = ParamIsRelative(program[instructionCounter], 10000);
+
+            switch (opcode)
+            {
+            case OpCode::Add:
+            {
+                const auto param1 = firstParamImmediate ? program[instructionCounter + 1] :
+                                    firstParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 1]] :
+                                    program[program[instructionCounter + 1]];
+                const auto param2 = secondParamImmediate ? program[instructionCounter + 2] :
+                                    secondParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 2]] :
+                                    program[program[instructionCounter + 2]];
+                const auto destination = thirdParamRelative ? state.relativeBasePointer + program[instructionCounter + 3] : program[instructionCounter + 3];
+                program[destination] = param1 + param2;
+                instructionCounter += 4;
+                break;
+            }
+            case OpCode::Multiply:
+            {
+                const auto param1 = firstParamImmediate ? program[instructionCounter + 1] :
+                                    firstParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 1]] :
+                                    program[program[instructionCounter + 1]];
+                const auto param2 = secondParamImmediate ? program[instructionCounter + 2] :
+                                    secondParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 2]] :
+                                    program[program[instructionCounter + 2]];
+                const auto destination = thirdParamRelative ? state.relativeBasePointer + program[instructionCounter + 3] : program[instructionCounter + 3];
+                program[destination] = param1 * param2;
+                instructionCounter += 4;
+                break;
+            }
+            case OpCode::Input:
+            {
+                const auto destination = firstParamRelative ? state.relativeBasePointer + program[instructionCounter + 1] : program[instructionCounter + 1];
+                program[destination] = inputs[inputCount++];
+                instructionCounter += 2;
+                break;
+            }
+            case OpCode::Output:
+            {
+                const auto param1 = firstParamImmediate ? program[instructionCounter + 1] :
+                                    firstParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 1]] :
+                                    program[program[instructionCounter + 1]];
+                result.emplace_back(param1);
+                instructionCounter += 2;
+                // Guess - return when second output is reached
+                if (result.size() == 2) { return result; }
+                break;
+            }
+            case OpCode::JumpIfTrue:
+            {
+                const auto param1 = firstParamImmediate ? program[instructionCounter + 1] :
+                                    firstParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 1]] :
+                                    program[program[instructionCounter + 1]];
+                const auto param2 = secondParamImmediate ? program[instructionCounter + 2] :
+                                    secondParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 2]] :
+                                    program[program[instructionCounter + 2]];
+                if (param1 != 0)
+                {
+                    instructionCounter = (uint64_t)param2;
+                }
+                else
+                {
+                    instructionCounter += 3;
+                }
+                break;
+            }
+            case OpCode::JumpIfFalse:
+            {
+                const auto param1 = firstParamImmediate ? program[instructionCounter + 1] :
+                                    firstParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 1]] :
+                                    program[program[instructionCounter + 1]];
+                const auto param2 = secondParamImmediate ? program[instructionCounter + 2] :
+                                    secondParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 2]] :
+                                    program[program[instructionCounter + 2]];
+                if (param1 == 0)
+                {
+                    instructionCounter = (uint64_t)param2;
+                }
+                else
+                {
+                    instructionCounter += 3;
+                }
+                break;
+            }
+            case OpCode::LessThan:
+            {
+                const auto param1 = firstParamImmediate ? program[instructionCounter + 1] :
+                                    firstParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 1]] :
+                                    program[program[instructionCounter + 1]];
+                const auto param2 = secondParamImmediate ? program[instructionCounter + 2] :
+                                    secondParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 2]] :
+                                    program[program[instructionCounter + 2]];
+                const auto destination = thirdParamRelative ? state.relativeBasePointer + program[instructionCounter + 3] : program[instructionCounter + 3];
+                program[destination] = (param1 < param2) ? 1 : 0;
+                instructionCounter += 4;
+                break;
+            }
+            case OpCode::Equals:
+            {
+                const auto param1 = firstParamImmediate ? program[instructionCounter + 1] :
+                                    firstParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 1]] :
+                                    program[program[instructionCounter + 1]];
+                const auto param2 = secondParamImmediate ? program[instructionCounter + 2] :
+                                    secondParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 2]] :
+                                    program[program[instructionCounter + 2]];
+                const auto destination = thirdParamRelative ? state.relativeBasePointer + program[instructionCounter + 3] : program[instructionCounter + 3];
+                program[destination] = (param1 == param2) ? 1 : 0;
+                instructionCounter += 4;
+                break;
+            }
+            case OpCode::AdjustRelativeBase:
+            {
+                const auto param1 = firstParamImmediate ? program[instructionCounter + 1] :
+                                    firstParamRelative ? program[state.relativeBasePointer + program[instructionCounter + 1]] :
+                                    program[program[instructionCounter + 1]];
+                state.relativeBasePointer += param1;
+                instructionCounter += 2;
+                break;
+            }
+            default:
+                std::cerr << "program[0] at time of crash = " << program[0] << std::endl;
+                throw std::exception("Invalid opcode");
+            }
+        }
+        return result;
+    }
+}
