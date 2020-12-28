dc7c1fb4b5aa39473526da9f7b550a1fb77550d1
David Machaj <46852402+dmachaj@users.noreply.github.com>
2020-12-27 22:59:26 -0800

    Add helper script to re-check every problem using intcode

---
+++ 2019/Intcode/check-all.cmd
@@ -0,0 +1,55 @@
+pushd ..\day2
+echo Checking day 2
+call ..\..\build.cmd
+call ..\..\verify.cmd Part1
+call ..\..\verify.cmd Part2
+popd
+
+pushd ..\day5
+echo Checking day 5
+call ..\..\build.cmd
+call ..\..\verify.cmd Part1
+call ..\..\verify.cmd Part2
+popd
+
+pushd ..\day7
+echo Checking day 7
+call ..\..\build.cmd
+call ..\..\verify.cmd Part1
+call ..\..\verify.cmd Part2
+popd
+
+pushd ..\day9
+echo Checking day 9
+call ..\..\build.cmd
+call ..\..\verify.cmd Part1
+call ..\..\verify.cmd Part2
+popd
+
+@REM pushd ..\day11
+@REM echo Checking day 11
+@REM call ..\..\build.cmd
+@REM call ..\..\verify.cmd Part1
+@REM call ..\..\verify.cmd Part2
+@REM popd
+
+@REM pushd ..\day13
+@REM echo Checking day 13
+@REM call ..\..\build.cmd
+@REM call ..\..\verify.cmd Part1
+@REM call ..\..\verify.cmd Part2
+@REM popd
+
+pushd ..\day17
+echo Checking day 17
+call ..\..\build.cmd
+call ..\..\verify.cmd Part1
+call ..\..\verify.cmd Part2
+popd
+
+pushd ..\day19
+echo Checking day 19
+call ..\..\build.cmd
+call ..\..\verify.cmd Part1
+call ..\..\verify.cmd Part2
+popd
