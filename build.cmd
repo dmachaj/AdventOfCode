45deea9048e1f2a92a3ea949bdf7543a3068d46b
David Machaj <46852402+dmachaj@users.noreply.github.com>
2021-11-23 10:55:22 -0800

    Update C++ standard to cpp20

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
-cl /EHsc /nologo /W4 /WX /std:c++17 /Zi /F 8000000 /I %~dp0Includes main.cpp
+cl /EHsc /nologo /W4 /WX /std:c++20 /Zi /F 8000000 /I %~dp0Includes main.cpp
 :: /O2 max speed
 :: /F 8000000 increase stack size to 8MB from 1MB to make recursion problems a little bit easier
