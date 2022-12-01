b87fc30c0305738645bb64d810b52246a154b6c2
David Machaj <46852402+dmachaj@users.noreply.github.com>
2022-11-30 20:52:11 -0800

    Stash more work in progress for 2021.19.01

--- 2021/Day19/main.cpp
+++ 2021/Day19/main.cpp
@@ -139,6 +139,18 @@ namespace
         std::vector<Beacon> beacons;
     };
 
+    std::vector<Scanner> ParseInput(std::istream& inStream)
+    {
+        std::vector<Scanner> scanners;
+
+        std::string input;
+        while (std::getline(inStream, input))
+        {
+            scanners.emplace_back(Scanner{input, inStream});
+        }
+        return scanners;
+    }
+
     void TEST()
     {
         assert((Beacon{"404,-588,-901"} == Beacon{404, -588, -901}));
@@ -168,17 +180,70 @@ namespace
 
         const auto match = zero.TryMatch(one, 3UL);
         assert((match == Beacon{5, 2, 0}));
+
+        std::string sampleInput = R"(--- scanner 0 ---
+404,-588,-901
+528,-643,409
+-838,591,734
+390,-675,-793
+-537,-823,-458
+-485,-357,347
+-345,-311,381
+-661,-816,-575
+-876,649,763
+-618,-824,-621
+553,345,-567
+474,580,667
+-447,-329,318
+-584,868,-557
+544,-627,-890
+564,392,-477
+455,729,728
+-892,524,684
+-689,845,-530
+423,-701,434
+7,-33,-71
+630,319,-379
+443,580,662
+-789,900,-551
+459,-707,401
+
+--- scanner 1 ---
+686,422,578
+605,423,415
+515,917,-361
+-336,658,858
+95,138,22
+-476,619,847
+-340,-569,-846
+567,-361,727
+-460,603,-452
+669,-402,600
+729,430,532
+-500,-761,534
+-322,571,750
+-466,-666,-811
+-429,-592,574
+-355,545,-477
+703,-491,-529
+-328,-685,520
+413,935,-424
+-391,539,-444
+586,-435,557
+-364,-763,-893
+807,-499,-711
+755,-354,-619
+553,889,-390
+)";
+        std::istringstream inStream{ sampleInput };
+        auto scanners = ParseInput(inStream);
+
+        assert(scanners[0].TryMatch(scanners[1], 12UL).has_value());
     }
 
     void Part1()
     {
-        std::vector<Scanner> scanners;
-
-        std::string input;
-        while (std::getline(std::cin, input))
-        {
-            scanners.emplace_back(Scanner{input, std::cin});
-        }
+        auto scanners = ParseInput(std::cin);
 
         Scanner world{99, scanners[0].beacons};
 
