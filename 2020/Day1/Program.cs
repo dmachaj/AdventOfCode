81518b925ecc20a4114d3cb82e018cb10a3bc8c9
David Machaj <46852402+dmachaj@users.noreply.github.com>
2024-08-30 18:08:19 -0700

    Solve 2020.01 in C#

--- 2020/Day1/Program.cs
+++ 2020/Day1/Program.cs
@@ -4,7 +4,26 @@ public class Program : IProgram
 {
     public void Part1()
     {
-        Console.WriteLine("0");
+        List<int> numbers = new List<int>();
+        string? line;
+        while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+        {
+            numbers.Add(int.Parse(line));
+        }
+
+        foreach (int n in numbers)
+        {
+            foreach (int m in numbers)
+            {
+                if (n ==m ) continue;
+
+                if ((n + m) == 2020)
+                {
+                    Console.WriteLine($"{n * m}");
+                    return;
+                }
+            }
+        }
     }
 
     public void Part2()
